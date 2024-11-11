9fc9fa3f0e4d45bec5871f62a4feb25a041eecfa
Gramandrei <andrei.gabriel.grama@stud.ubbcluj.ro>
2024-11-11 16:31:31 +0200

    Added abstract class User, Host and Guest

---
+++ AirBnb/src/Host.java
@@ -0,0 +1,22 @@
+public class Host extends User {
+    private double hostRating;
+
+    public Host(int userID, String name, String email, String phone, double hostRating) {
+        super(userID, name, email, phone);
+        this.hostRating = hostRating;
+    }
+
+    public void listProperty() {
+    }
+
+    public void viewBookings() {
+    }
+
+    public double getHostRating() {
+        return hostRating;
+    }
+
+    public void setHostRating(double hostRating) {
+        this.hostRating = hostRating;
+    }
+}
---
+++ AirBnb/src/User.java
@@ -0,0 +1,45 @@
+public abstract class User {
+    protected int userID;
+    protected String name;
+    protected String email;
+    protected String phone;
+
+    public User(int userID, String name, String email, String phone) {
+        this.userID = userID;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
